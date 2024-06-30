ca97f66fab86f1c6c5952037d186d3d6916efed6
Radheshyam Gupta <radheshyamgupta@ternaengg.ac.in>
2024-06-30 13:43:30 +0530

    add all file

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//React Element ==> Object ==> HTML Element(render)
 
-const parent =React.createElement("div", {id:"parent"},[
-     React.createElement("div", {id:"child"},[
-        React.createElement("h1",{},"This is My first app"),
-        React.createElement("h2",{},"Create by Radhe"),
-]),
-React.createElement("div", {id:"child2"},[
-    React.createElement("h1",{},"I'm an h1 tag"),
-    React.createElement("h2",{},"I'm an h1 tag"),
-]),
-]);
+const heading = React.createElement("h1",{id:"heading"},"Radhe Project");
 
-console.log(parent);
-const root= ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
